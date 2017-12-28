253b2598e7bff40c96c09aab259ae23509fb2e13
kth2242 <34909594+kth2242@users.noreply.github.com>
2017-12-28 18:16:47 +0900

    LaLaLa Test1
    test commiting

---
+++ Assets/Shot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+    public float Power = 200f;
+    public float Range = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(transform.position, this.transform.up * Range, Color.red);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.up, out hit, Range))
+            {
+                Debug.Log("shoot");
+                if (hit.collider.gameObject.tag == "Cube")
+                {
+                    
+                    hit.rigidbody.AddForceAtPosition(transform.up * Power, hit.point);
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float totalTime = 60f;
+    private int showTime = 0;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        showTime = (int)totalTime;
+        totalTime -= Time.deltaTime;
+
+        gameObject.GetComponent<UnityEngine.UI.Text>().text = "Time Left : " + showTime.ToString();
+    }
+}
