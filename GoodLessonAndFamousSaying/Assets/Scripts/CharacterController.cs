af2ccc32b4a2107b8cdf7cd39fbc7b579868166a
Picbridge <34910988+Picbridge@users.noreply.github.com>
2018-01-01 12:12:26 +0900

    CameraAndPlayerControll

---
+++ GoodLessonAndFamousSaying/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Hide the curser and lock the cursor inside the game
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        //Turning off the lock mode of the cursor (makes cursor visible)
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+}
---
+++ GoodLessonAndFamousSaying/Assets/Scripts/camMouseLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camMouseLook : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    public float sensitivity = 2f;
+    public float smoothing = 2f;
+
+    GameObject character;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Set target game object to it's parent
+        character = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Update only if it is first person view
+        if (this.GetComponent<Camera>().enabled)
+        {
+            //mouseDelta
+            var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+            md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+            smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+            smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+
+            mouseLook += smoothV;
+            mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+            transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+            character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        }
+	}
+}
