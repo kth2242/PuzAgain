e6c69f5b8dda4c08ec17bcfd9d4daa4851f2e03f
kth2242 <34909594+kth2242@users.noreply.github.com>
2018-01-02 01:37:58 +0900

    goblin animation movement 연구중

---
+++ Goblin/Assets/FollowDestination.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDestination : MonoBehaviour {
+
+	private UnityEngine.AI.NavMeshAgent thisAgent = null;
+	public Transform destination = null;
+
+	// Use this for initialization
+	void Awake () {
+		thisAgent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//thisAgent.enabled = false;
+		thisAgent.SetDestination (destination.position);
+		//thisAgent.enabled = true;
+	}
+}
